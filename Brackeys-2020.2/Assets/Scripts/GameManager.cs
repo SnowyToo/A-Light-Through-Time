49498a4771c00908510b5e9bc5e4c75acdf974a4
Snowy <devsnowgames@gmail.com>
2020-08-01 18:37:37 +0200

    Gameplay implemented

---
+++ Brackeys-2020.2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static Transform player;
+    public static Transform cam;
+    public static TimeManager timeManager;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        cam = GameObject.FindWithTag("MainCamera").transform;
+        timeManager = GetComponent<TimeManager>();
+    }
+}
