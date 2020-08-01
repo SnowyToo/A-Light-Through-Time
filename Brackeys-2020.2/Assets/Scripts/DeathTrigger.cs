7cc3aa53ccbc3d8bc0dbf092117ead5c562bfafa
SphaeriCactus <sphaericactus@gmail.com>
2020-08-01 12:22:05 -0700

    Added death

---
+++ Brackeys-2020.2/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DeathTrigger : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("enter");
+            StartCoroutine(GameManager.ReloadLevel());
+        }
+    }
+}
--- Brackeys-2020.2/Assets/Scripts/GameManager.cs
+++ Brackeys-2020.2/Assets/Scripts/GameManager.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static Transform player;
     public static Transform cam;
     public static TimeManager timeManager;
-    
-    // Start is called before the first frame update
+
     void Awake()
     {
         player = GameObject.FindWithTag("Player").transform;
         cam = GameObject.FindWithTag("MainCamera").transform;
         timeManager = GetComponent<TimeManager>();
+    }
+
+    public static IEnumerator ReloadLevel()
+    {
+        // Some sort of fade out/death animation
+        yield return new WaitForSeconds(0f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
