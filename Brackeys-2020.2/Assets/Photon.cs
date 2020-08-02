e41eaac72684bf4d597bb3e502fe2afd24fc2875
SphaeriCactus <sphaericactus@gmail.com>
2020-08-01 20:14:24 -0700

    Photon bouncing

---
+++ Brackeys-2020.2/Assets/Photon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Photon : MonoBehaviour
+{
+    public float maxSpeed = 8f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        SetRandomVelocity();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = rb.velocity.normalized * maxSpeed;
+    }
+
+    void SetRandomVelocity()
+    {
+        float angle = Random.Range(0f, 2f * Mathf.PI);
+        rb.velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * maxSpeed;
+    }
+}
