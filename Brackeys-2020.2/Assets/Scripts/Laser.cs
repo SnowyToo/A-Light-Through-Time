83b46e4a0f7a1cee17ac4b2d5e983c7665d9aa0d
SphaeriCactus <sphaericactus@gmail.com>
2020-08-02 13:24:06 -0700

    Laser kills player

---
+++ Brackeys-2020.2/Assets/Scripts/Laser.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private int damage;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject other = col.gameObject;
+        if (other.CompareTag("Player"))
+        {
+            GameManager.player.TakeDamage(damage);
+        }
+    }
+}
