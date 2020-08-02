9e03c11d596b83e318495ce5d40fdc464740f291
SphaeriCactus <sphaericactus@gmail.com>
2020-08-01 19:43:20 -0700

    Movement (trash) and aiming

--- Brackeys-2020.2/Assets/Scripts/PlayerMovement.cs
+++ Brackeys-2020.2/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Movement
     public float speed = 5f;
     private Vector2 movement;
     private Rigidbody2D rb;
+
+    // Aiming
+    private Camera cam;
+    private Vector2 mousePosition;
+
     void Awake()
     {
+        cam = Camera.main;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -17,11 +24,15 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         movement = new Vector2(horizontal, vertical).normalized;
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
-        //rb.AddImpulse(movement * speed);
+        rb.MovePosition(rb.position + (movement * speed * Time.fixedDeltaTime));
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 }
