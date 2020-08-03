3c92c26e03b7f979e87238b60742d68e97941f7b
Snowy <devsnowgames@gmail.com>
2020-08-03 17:16:53 +0200

    Shield Boop and Shield Pop

--- Brackeys-2020.2/Assets/Scripts/Shield.cs
+++ Brackeys-2020.2/Assets/Scripts/Shield.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
+    [HideInInspector]
     public Enemy parent;
+
+    [SerializeField]
+    GameObject shieldPop;
+    [SerializeField]
+    AudioClip shieldBoop;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.collider.tag == "Photon" && !parent.invincible)
         {
             parent.ShieldHit();
+
+            GameManager.PlaySound(shieldBoop, gameObject);
+            GameManager.SpawnParticles(shieldPop, gameObject);
+
             Destroy(gameObject);
         }
     }
